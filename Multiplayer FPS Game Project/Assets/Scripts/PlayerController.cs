58623fa2ee8cf40112bf0743217ee9d473f93e5e
Kshitijv24 <kshitijv24@gmail.com>
2023-05-04 10:30:44 +0530

    Added Player Movement in the game

--- Multiplayer FPS Game Project/Assets/Scripts/PlayerController.cs
+++ Multiplayer FPS Game Project/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform viewPoint;
     [SerializeField] float mouseSensitivity = 1f;
+    [SerializeField] float moveSpeed = 1f;
 
     float mouseVerticalRotation;
     Vector2 mouseInput;
+    Vector3 moveDirection, movement;
 
     private void Start()
     {
@@ -16,6 +18,12 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update()
+    {
+        HandlePlayerMouseMovement();
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMouseMovement()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
@@ -31,5 +39,14 @@ public class PlayerController : MonoBehaviour
             -mouseVerticalRotation,
             viewPoint.rotation.eulerAngles.y,
             transform.rotation.eulerAngles.z);
+    }
+
+    private void HandlePlayerMovement()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized;
+
+        transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
