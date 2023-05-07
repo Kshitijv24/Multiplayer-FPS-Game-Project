46595a545aeb833dd4103710bd8adad7d38a6bc6
Kshitijv24 <kshitijv24@gmail.com>
2023-05-07 21:37:22 +0530

    Setup the Gun models in the project

---
+++ Multiplayer FPS Game Project/Assets/Scripts/Gun.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] bool isAutomatic;
+    [SerializeField] float timeBetweenShots;
+    [SerializeField] float heatPerShot;
+}
